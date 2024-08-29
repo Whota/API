4646634758805f8303db6afcde1eaacb8a28dba3
Michael G. Tellez II <michaeltellez93@gmail.com>
2024-08-29 16:43:42 -0500

    Initial Database Migrations

--- TestMVC/Program.cs
+++ TestMVC/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlite("Connection string");
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
